c6861b7e745c8c9a2b7ba6760124f65c3d7e0eea
60manojkumar@gmail.com <Motivated@2019>
2022-07-15 19:27:41 +0530

    CTRL+K -- COMMIT

--- src/demo_variables/GitDemo.java
+++ src/demo_variables/GitDemo.java
@@ -4,6 +4,9 @@ public class GitDemo {
 
     public static void main(String[] args) {
 
+
+
+        System.out.println("SECOND TEXT This class is for GIT DEMO");
         System.out.println("This class is for GIT DEMO");
     }
 }
