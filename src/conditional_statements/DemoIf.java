b4b065b2ce8499c93697116c5f4b724e0cf794a8
60manojkumar@gmail.com <Motivated@2019>
2022-07-16 19:29:03 +0530

    conditional statements

---
+++ src/conditional_statements/DemoIf.java
@@ -0,0 +1,30 @@
+package conditional_statements;
+
+public class DemoIf {
+
+    public static void main(String[] args) {
+
+        //TODO  SYNTAX:  if(condition){CODE}
+
+        int age=10;
+
+        if(age>18){
+
+            System.out.println("Person is eligible for voting .....");
+        }
+        if(age<18){
+
+            System.out.println("Person is Child so not eligible for voting ...");
+        }
+
+
+        String city="AGRA";
+
+        if (city=="AGRA"){
+
+            System.out.println("Person belongs to AGRA");
+        }
+
+    }
+
+}
---
+++ src/conditional_statements/DemoIfAndElse.java
@@ -0,0 +1,30 @@
+package conditional_statements;
+
+public class DemoIfAndElse {
+
+    public static void main(String[] args) {
+
+        int age=10;
+
+        if (age>18){
+
+            System.out.println("Person is eligible for voting ..");
+        }
+        else {
+
+            System.out.println("Person is not eligible for voting ..");
+        }
+
+
+        String city="DELHI";
+
+        if(city=="AGRA"){
+
+            System.out.println(" city is AGRA ...");
+        }
+        else {
+            System.out.println("City is not agra .....");
+        }
+
+    }
+}
