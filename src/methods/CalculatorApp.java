d7e283671d355107b64e931398f76ef29f120da8
60manojkumar@gmail.com <Motivated@2019>
2022-08-06 19:37:10 +0530

    method Day 5

--- src/methods/CalculatorApp.java
+++ src/methods/CalculatorApp.java
@@ -27,27 +27,40 @@ public class CalculatorApp {
 
 
 
-        System.out.println("Please enter value of a :");
-        int a= scanner.nextInt();// reads integer value
-
-        System.out.println("Please enter value of b :");
-
-        int b= scanner.nextInt();
-
-        System.out.println("Please enter your choice : 1-Add, 2-Sub, 3-Multi and 4-Divide");
-        int choice= scanner.nextInt();
 
         CalculatorApp obj= new CalculatorApp();
 
-        if(choice==1){
-            int result= obj.addition(a,b);
-            System.out.println("Addition is "+result);
-        }
-        else if(choice==2){
-            int result=obj.subtraction(a,b);
-            System.out.println("Subtraction is "+result);
+        String flag="Y";
 
+        while(flag.equals("Y")){
+
+
+            System.out.println("Please enter your choice : 1-Add, 2-Sub, 3-Multi and 4-Divide");
+            int choice= scanner.nextInt();
+
+            System.out.println("Please enter value of a :");
+            int a= scanner.nextInt();// reads integer value
+
+            System.out.println("Please enter value of b :");
+
+            int b= scanner.nextInt();
+
+            if(choice==1){
+                int result= obj.addition(a,b);
+                System.out.println("Addition is "+result);
+            }
+            else if(choice==2){
+                int result=obj.subtraction(a,b);
+                System.out.println("Subtraction is "+result);
+
+            }
+
+            System.out.println("Please press Y to continue Calc or press any button to exit");
+            Scanner scanner2= new Scanner(System.in);
+            flag=scanner2.nextLine();
         }
+
+
 
 
 
