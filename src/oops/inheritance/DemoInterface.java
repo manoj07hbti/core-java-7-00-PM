c9e4548107766952ae84f09398e5cf383b91c104
60manojkumar@gmail.com <Motivated@2019>
2022-08-24 19:32:15 +0530

    interface

---
+++ src/oops/inheritance/DemoInterface.java
@@ -0,0 +1,8 @@
+package oops.inheritance;
+
+public interface DemoInterface {
+    //Syntax : access_specifier abstract return_type  method_name (parameter) ;
+    public void m1();
+    public void m2();
+
+}
